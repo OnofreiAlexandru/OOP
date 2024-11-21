ecf0384c529f59b454eeef79aa4b4d6f2594dd3e
OnofreiAlexandru <onofrei.alexandru.t8t@student.ucv.ro>
2024-11-21 12:36:28 +0200

    Add files via upload

---
+++ Lab1 homework/Lab1 homework/header1.h
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+#ifndef HEADER1_H
+#define HEADER1_H
+
+void f() {
+	cout << "Function f() from header1.h" << std::endl;
+}
+
+#endif
+
+
---
+++ Lab1 homework/Lab1 homework/header2.h
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+#ifndef HEADER2_H
+#define HEADER2_H
+
+void f() {
+	cout << "Function f() from header2.h" << std::endl;
+}
+
+#endif
